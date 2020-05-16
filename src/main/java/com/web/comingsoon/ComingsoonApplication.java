04fd6a020bedebe71e08b5c833d22a53bd4b0c50
stage41 <stage41@nate.com>
2020-05-17 00:36:29 +0900

    Initial commit

---
+++ src/main/java/com/web/comingsoon/ComingsoonApplication.java
@@ -0,0 +1,15 @@
+package com.web.comingsoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComingsoonApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ComingsoonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/comingsoon/ComingsoonApplicationTests.java
@@ -0,0 +1,15 @@
+package com.web.comingsoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComingsoonApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
